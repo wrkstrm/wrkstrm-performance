92dad05bca3791548188f3119f33dd734be8c20b
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2025-08-12 20:07:34 -0700

    Remove WSMBootTimeMonitor and boot-time hooks

--- Sources/WrkstrmPerformanceObjC/MachTimeAnalyzer.m
+++ Sources/WrkstrmPerformanceObjC/MachTimeAnalyzer.m
@@ -1,5 +1,4 @@
 #import "MachTimeAnalyzer.h"
-#import "WSMBootTimeMonitor.h"
 
 uint64_t kGlobalStartTime = 0; // Define the global variable
 
@@ -19,18 +18,10 @@ extern uint64_t WSMGetGlobalStartTime(void) {
   return kGlobalStartTime;
 }
 
-__attribute__((constructor))
-static void GymkhanaConstructor(void) {
-  // Call into Swift code via selector to mark timestamp
-  [WSMBootTimeMonitor markEarlyTimestamp:@"monitor_static_constructor"];
-}
-
 @implementation MachTimeAnalyzer
 
 + (void)load {
   InitializeGlobalStartTime();
-  // This method is called when the class is loaded into the runtime
-  [WSMBootTimeMonitor markEarlyTimestamp:@"monitor_class_load"];
 }
 
 @end
