d10404959309744c04fc8f5c172d49113bb0e158
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2025-08-12 21:09:33 -0700

    chore: update WrkstrmLog to 2.0.0

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ Package.Inject.local.dependencies = [
 ]
 
 Package.Inject.remote.dependencies = [
-  .package(url: "https://github.com/wrkstrm/WrkstrmLog.git", from: "1.0.0")
+  .package(url: "https://github.com/wrkstrm/WrkstrmLog.git", from: "2.0.0")
 ]
 #if !os(Linux)
   Package.Inject.remote.dependencies.append(
