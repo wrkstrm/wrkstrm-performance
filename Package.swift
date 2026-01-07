6b8916c095a455d51af6b67f426c35b2e9715430
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2026-01-07 02:58:27 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ Package.Inject.local.dependencies = [
 Package.Inject.remote.dependencies = [
   .package(url: "https://github.com/wrkstrm/WrkstrmLog.git", from: "2.0.0"),
   .package(url: "https://github.com/wrkstrm/WrkstrmMain.git", from: "2.4.0"),
-  .package(url: "https://github.com/wrkstrm/WrkstrmFoundation.git", from: "2.0.0"),
+  .package(url: "https://github.com/wrkstrm/WrkstrmFoundation.git", from: "3.0.0"),
 ]
 
 var packageProducts: [Product] = [
